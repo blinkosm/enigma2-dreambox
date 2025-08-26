38b5df56d35e8f38920b033775b5e2c80456b567
blinkosm <blinkosm@gmail.com>
2025-08-26 17:57:37 +0100

    Update epgtransponderdatareader.h

--- lib/dvb/epgtransponderdatareader.h
+++ lib/dvb/epgtransponderdatareader.h
@@ -4,7 +4,7 @@
 /* Restart EPG data capture */
 #define UPDATE_INTERVAL 700  // 60 min
 /* Time to wait after tuning in before EPG data capturing starts */
-#define ZAP_DELAY 2000         // 2 sec
+#define ZAP_DELAY 700        // 2 sec
 
 #include <tr1/unordered_map>
 
